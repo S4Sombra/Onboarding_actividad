e5f8a03947640aa9fc5c476c7b6dfea39f863331
sombra <sombra9611@gmail.com>
2023-08-07 08:04:46 +0200

    Step01: Primer commit con Onboarding_layout realizado como paso intermedio antes de completar la Home_screen

---
+++ app/src/main/java/es/travelworld/onboarding/MainActivity.java
@@ -0,0 +1,19 @@
+package es.travelworld.onboarding;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ViewPager viewPager = findViewById(R.id.viewPager);
+
+
+    }
+}
